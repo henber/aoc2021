97d78e45e61c9b7022971a928235cac1af68f51d
Henrik Berglund <henrik.berglund@leovegas.com>
2021-12-07 12:01:30 +0100

    Cleaned up day7 a bit.

--- src/main/kotlin/day7/day7.kt
+++ src/main/kotlin/day7/day7.kt
@@ -9,27 +9,15 @@ val maxCrab = input.maxOf { it }
 
 fun fuelConsumption(crab: Int, target: Int): Int = abs(crab - target)
 
-fun solveA(): Int {
-    var minValue = Int.MAX_VALUE
-    for (i in 0..maxCrab) {
-        val sumFuel = input.sumOf { fuelConsumption(it, i) }
-        if (sumFuel < minValue) {
-            minValue = sumFuel
-        }
-    }
-
-    return minValue
-}
-
 fun fuelConsumption2(crab: Int, target: Int): Int {
     val rawDist = abs(crab - target)
     return rawDist * (rawDist + 1) / 2
 }
 
-fun solveB(): Int {
+fun findMinFuel(calcFuel: (Int, Int) -> Int): Int {
     var minValue = Int.MAX_VALUE
     for (i in 0..maxCrab) {
-        val sumFuel = input.sumOf { fuelConsumption2(it, i) }
+        val sumFuel = input.sumOf { calcFuel(it, i) }
         if (sumFuel < minValue) {
             minValue = sumFuel
         }
@@ -37,6 +25,10 @@ fun solveB(): Int {
 
     return minValue
 }
+
+fun solveA(): Int = findMinFuel(::fuelConsumption)
+
+fun solveB(): Int = findMinFuel(::fuelConsumption2)
 
 fun main() {
     println("Answer A: ${solveA()}")
